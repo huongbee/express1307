01b2b4d78c9709cfa8ea7b2092c2fc87974035a9
Huong <huongnguyenak96@gmail.com>
2019-07-28 11:59:23 +0700

    try catch

--- index.js
+++ index.js
@@ -8,8 +8,34 @@ app.get('/', (req, res) => {
 })
 
 
-app.get('/detail/:page', (req, res) => {
-    // const alias = req.param('slug', 'default')
+app.get('/detail/:page?', (req, res) => {
     const page = req.params.page || 1
     res.send({ page })
 })
+class Calculator {
+    constructor(a, b, pt) {
+        this.a = a;
+        this.b = b;
+        this.pt = pt
+    }
+    get pheptinh() {
+        if (this.pt == 'cong') return '+'
+        if (this.pt == 'tru') return '-'
+        if (this.pt == 'nhan') return '*'
+        if (this.pt == 'chia') return '/'
+        if (this.pt == 'chiadu') return '%'
+        throw new Error('Undefined pt!')
+    }
+    get result() {
+        let str = `${this.a} ${this.pheptinh} ${this.b}`
+        // `2 + 4`
+        return eval(str)
+    }
+}
+try {
+    const cal = new Calculator(2, 'qqq', 'tru')
+    console.log(cal.result)
+}
+catch (error) {
+    console.log(error.message)
+}
